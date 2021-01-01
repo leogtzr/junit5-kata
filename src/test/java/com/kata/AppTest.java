c20424179c8aaa6fd3d0d6021da227a3015d91f7
leogtzr <leogutierrezramirez@gmail.com>
2020-12-31 20:35:00 -0700

    first commit

---
+++ src/test/java/com/kata/AppTest.java
@@ -0,0 +1,12 @@
+package com.kata;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    @DisplayName("Some test ... ")
+    public void foo() {
+        System.out.println("Hello ... ");
+    }
+}
